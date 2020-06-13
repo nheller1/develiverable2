a868b48e0b4f3733030783eb30b16290cf3cfc3b
Nikida Heller <53826007+nheller1@users.noreply.github.com>
2020-06-13 11:29:57 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var vacationType=prompt('What type of trip would you like to go on, musical, tropical, or adventurous?');
+console.log(vacationType); {
+
+if (vacationType==="musical") {
+    var group size = prompt ( 'How many are in your group?')
+    console.log(groupSize);
+
+      if (groupSize==="1" || groupSize === '2') [
+        console.log(groupSize);
+    ]
+else if (vacationType==="tropical") {
+  prompt = ('How many are in your group?')
+}
+else if (vacationType==="adventurous") {
+  prompt = ('How many are in your group?')
+}
+
+result = concat( "Since you're a group of", groupSize, "you should", travelType, "to", destination)
+
+var groupSize = prompt ()
+var travelType = prompt ()
+var destination = prompt ()
+let result;
