85ce88d363804aba4fefc0f3088f9598b2f87264
Nikida Heller <53826007+nheller1@users.noreply.github.com>
2020-06-20 11:58:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 var vacationType=prompt('What type of trip would you like to go on, musical, tropical, or adventurous?');
 console.log(vacationType); {
 
-if (vacationType==="musical") {
-    var group size = prompt ( 'How many are in your group?')
-    console.log(groupSize);
-
-      if (groupSize==="1" || groupSize === '2') [
-        console.log(groupSize);
-    ]
-else if (vacationType==="tropical") {
-  prompt = ('How many are in your group?')
+if(vacationType === "musical") {if (groupSize === '1' || groupSize === '2') {
+   alert (result)
 }
-else if (vacationType==="adventurous") {
-  prompt = ('How many are in your group?')
+   prompt (groupSize == "2")
 }
-
-result = concat( "Since you're a group of", groupSize, "you should", travelType, "to", destination)
-
-var groupSize = prompt ()
-var travelType = prompt ()
+    
+else if (vacationType === "tropical") {
+    prompt (groupSize == "2")
+}
+else if (vacationTyoe === "adventerous") {
+    prompt (groupSize == "2")
+}
+    
+alert (result)
+    
+var groupSize = prompt ("How many are in your group?"
+var travelType = "take a first class flight"
+                        
 var destination = prompt ()
 let result;
+    
+var str1 = "Since you're a group of"
+var str2 = " you should "
+var str3 = " to ";
+    
+result = str1.concat (groupSize, str2, travelType, str3)
+
+if (groupSize === "1") (return "take a first class flight")
+    else if (groupSize === "3") {return "take a helicopter"}
+    else if(groupSize >= (6)) {return " take a charter flight"}
